9b747d8a47888f9ea2b273ca4f992b4f603ea2de
Jon Friesen <jfriesen@digitalocean.com>
2021-09-22 08:06:17 -0700

    downgrade go version to 1.15

--- packages/default/stonks/go.mod
+++ packages/default/stonks/go.mod
@@ -1,6 +1,6 @@
 module github.com/jonfriesen/nim-stonks
 
-go 1.17
+go 1.15
 
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
--- packages/default/stonks/stonks.go
+++ packages/default/stonks/stonks.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/jonfriesen/finance-go"
@@ -27,31 +25,31 @@ type YQuote struct {
 }
 
 // This main function is for testing locally
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		qp := r.URL.Query()
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		qp := r.URL.Query()
 
-		params := make(map[string]interface{}, len(qp))
-		for k, v := range qp {
-			params[k] = v
-		}
-		w.Header().Set("Content-Type", "text/html")
-		payload := Main(params)
-		if b, ok := payload["body"]; ok {
-			fmt.Fprint(w, b)
-		}
-		if b, ok := payload["error"]; ok {
-			fmt.Fprint(w, b)
-		}
-	})
+// 		params := make(map[string]interface{}, len(qp))
+// 		for k, v := range qp {
+// 			params[k] = v
+// 		}
+// 		w.Header().Set("Content-Type", "text/html")
+// 		payload := Main(params)
+// 		if b, ok := payload["body"]; ok {
+// 			fmt.Fprint(w, b)
+// 		}
+// 		if b, ok := payload["error"]; ok {
+// 			fmt.Fprint(w, b)
+// 		}
+// 	})
 
-	// push up to web?
-	// fs := http.FileServer(http.Dir("static/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+// 	// push up to web?
+// 	// fs := http.FileServer(http.Dir("static/"))
+// 	// http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	fmt.Println("Running on port :8080")
-	http.ListenAndServe(":8080", nil)
-}
+// 	fmt.Println("Running on port :8080")
+// 	http.ListenAndServe(":8080", nil)
+// }
 
 func Main(args map[string]interface{}) map[string]interface{} {
 	symbol, ok := args["symbol"].(string)
